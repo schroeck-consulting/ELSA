4b508975764f57139293635504106db5fe0aafea
Anna <anna.gunaza@gmail.com>
2024-09-20 11:51:54 +0200

    changed thread parameter  to tread_id

--- assistant_api.py
+++ assistant_api.py
@@ -12,28 +12,28 @@ class AssistantClient:
         return self.client.beta.threads.create()
     
 
-    def submit_message(self, thread, user_message: str):
+    def submit_message(self, thread_id, user_message: str):
         self.client.beta.threads.messages.create(
-            thread_id=thread.id, role="user", content=user_message
+            thread_id=thread_id, role="user", content=user_message
         )
         return self.client.beta.threads.runs.create(
-            thread_id=thread.id,
+            thread_id=thread_id,
             assistant_id=self.assistant_id,
         )
     
 
-    def wait_on_run(self, run, thread):
+    def wait_on_run(self, run, thread_id):
         while run.status == "queued" or run.status == "in_progress":
             run = self.client.beta.threads.runs.retrieve(
-                thread_id=thread.id,
+                thread_id=thread_id,
                 run_id=run.id,
             )
             time.sleep(0.5)
         return run
     
 
-    def get_response_messages(self, thread):
-        return self.client.beta.threads.messages.list(thread_id=thread.id, order="asc")
+    def get_response_messages(self, thread_id):
+        return self.client.beta.threads.messages.list(thread_id=thread_id, order="asc")
     
 
     def extract_assistant_response(self, messages) -> str:
