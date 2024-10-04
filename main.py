b8baaf103aad14c87e00df3ccd1388a2c1727f77
Anna <anna.gunaza@gmail.com>
2024-10-04 16:01:32 +0200

    login form is on the same page as chatbot, it disappears after successful authentication

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from assistant_handlers import handle_predefined_questions, handle_user_queries
 from session_utils import init_session_state, init_assistant_client
+from auth import authenticate
 
 
 def display_logo():
@@ -19,33 +20,30 @@ def display_logo():
     st.sidebar.markdown("")
 
 
-def main():
-    display_logo()
-    # Check if the user is authenticated
-    if 'authentication_status' not in st.session_state:
-        st.switch_page('./pages/account.py')
+def display_chat_messages():
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
+
+def main():
+    authenticate()
+
+    # If the user is authenticated, show the main app content
     if st.session_state.authentication_status:
         init_session_state()
         init_assistant_client()
-                 
         
-        # Display the title
+        display_logo()
         st.title("ELSA - Epic Lifecycle Support Assistant")
-
-        # Display chat messages
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
+        display_chat_messages()
         
         # Handle predefined questions or chat input based on interaction state
         if not st.session_state.assistant_started:
             handle_predefined_questions()
         else:
             handle_user_queries()
-            
-    else:
-        st.write('Please log in on Account page.')
+
 
 if __name__ == "__main__":
     main()
