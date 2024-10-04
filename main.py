630c208e2d6578e803cfc1e056f70b9ad7db444c
Anna <anna.gunaza@gmail.com>
2024-10-04 13:17:44 +0200

    authentication in main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 from assistant_handlers import handle_predefined_questions, handle_user_queries
 from session_utils import init_session_state, init_assistant_client
 
+
 def display_logo():
     # To make logo bigger
     st.html("""
@@ -19,24 +20,32 @@ def display_logo():
 
 
 def main():
-    init_session_state()
-    init_assistant_client()
-    
-    display_logo()    
-    
-    # Display the title
-    st.title("ELSA - Epic Lifecycle Support Assistant")
+    display_logo()
+    # Check if the user is authenticated
+    if 'authentication_status' not in st.session_state:
+        st.switch_page('./pages/account.py')
 
-    # Display chat messages
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-    
-    # Handle predefined questions or chat input based on interaction state
-    if not st.session_state.assistant_started:
-        handle_predefined_questions()
+    if st.session_state.authentication_status:
+        init_session_state()
+        init_assistant_client()
+                 
+        
+        # Display the title
+        st.title("ELSA - Epic Lifecycle Support Assistant")
+
+        # Display chat messages
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+        
+        # Handle predefined questions or chat input based on interaction state
+        if not st.session_state.assistant_started:
+            handle_predefined_questions()
+        else:
+            handle_user_queries()
+            
     else:
-        handle_user_queries()
+        st.write('Please log in on Account page.')
 
 if __name__ == "__main__":
     main()
