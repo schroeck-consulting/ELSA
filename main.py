0aaf943660522308c48824dbfcc824997488ddb3
annagnz <98704148+annagnz@users.noreply.github.com>
2024-10-07 10:23:44 +0200

    Feature/streamlit authenticator (#4)
    * added config.yaml file
    
    * added account page with login, register and account tabs
    
    * authentication in main.py
    
    * login form is on the same page as chatbot, it disappears after successful authentication
    
    * added beta-access user

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from assistant_handlers import handle_predefined_questions, handle_user_queries
 from session_utils import init_session_state, init_assistant_client
+from auth import authenticate
+
 
 def display_logo():
     # To make logo bigger
@@ -18,25 +20,30 @@ def display_logo():
     st.sidebar.markdown("")
 
 
-def main():
-    init_session_state()
-    init_assistant_client()
-    
-    display_logo()    
-    
-    # Display the title
-    st.title("ELSA - Epic Lifecycle Support Assistant")
-
-    # Display chat messages
+def display_chat_messages():
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
-    
-    # Handle predefined questions or chat input based on interaction state
-    if not st.session_state.assistant_started:
-        handle_predefined_questions()
-    else:
-        handle_user_queries()
+
+
+def main():
+    authenticate()
+
+    # If the user is authenticated, show the main app content
+    if st.session_state.authentication_status:
+        init_session_state()
+        init_assistant_client()
+        
+        display_logo()
+        st.title("ELSA - Epic Lifecycle Support Assistant")
+        display_chat_messages()
+        
+        # Handle predefined questions or chat input based on interaction state
+        if not st.session_state.assistant_started:
+            handle_predefined_questions()
+        else:
+            handle_user_queries()
+
 
 if __name__ == "__main__":
     main()
